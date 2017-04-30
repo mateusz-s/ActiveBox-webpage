4fe2110aaada3872d2270edf98145f417b9f56cf
Mateusz Smykowski <msmykowski@interia.pl>
2017-04-30 21:21:13 +0200

    JS script

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+});
+    
+var navCheck = $('#navigation'),
+    navLabel = $('label > i');
+
+$('input').on('click', navIcon);
+
+function navIcon() {
+    if(navCheck.is(':checked')) {
+        navLabel.removeClass('fa-bars').addClass('fa-times');
+    } else {
+        navLabel.removeClass('fa-times').addClass('fa-bars');
+    }
+}
